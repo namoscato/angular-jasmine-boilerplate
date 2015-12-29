e71d752e34a8f69e43b4dd10ee7dc1fb40ffe165
Nick Amoscato <nicholas@amoscato.com>
2015-12-29 00:16:49 -0500

    Enable path configuration saving; add documentation

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # AngularJS Jasmine Boilerplate Generation
 
-Generates boilerplate [Jasmine](http://jasmine.github.io/) tests from [AngularJS](https://angularjs.org/) components via [Dgeni](https://github.com/angular/dgeni).
+Generates boilerplate [Jasmine](http://jasmine.github.io/) tests from [annotated](https://github.com/angular/angular.js/wiki/Writing-AngularJS-Documentation) [AngularJS](https://angularjs.org/) components via [Dgeni](https://github.com/angular/dgeni).
+
+## Usage
+
+    ./generate.js [--save] [--base-path=path] [--test-path=path] [file ...]
+
+1. Install dependencies:
+
+        npm install
+
+2. Save relevant paths:
+
+        ./generate.js --save --base-path=/dev/project/src --test-path=/dev/project/tests
+
+    where `base-path` is the base path to the project and `test-path` is the path to the project tests.
+
+3. Generate Jasmine boilerplate:
+
+        ./generate.js session/session.service.js
+
+    where the arguments are one or more paths to annotated AngularJS components, relative to `base-path`.
--- generate.js
+++ generate.js
@@ -1,25 +1,72 @@
 #!/usr/bin/env node
 
 var Dgeni = require('dgeni'),
+    jsonfile = require('jsonfile'),
     minimist = require('minimist');
 
-var options = minimist(process.argv.slice(2), {
+var config = {},
+    configFile,
+    dgeni,
+    options;
+
+options = minimist(process.argv.slice(2), {
+    boolean: 'save',
     string: [
         'base-path',
         'test-path'
     ]
 });
 
-try {
-    var dgeni = new Dgeni([
+jsonfile.spaces = 4;
+
+config.basePath = options['base-path'];
+config.testPath = options['test-path'];
+
+if (!config.basePath || !config.testPath) {
+    configFile = jsonfile.readFileSync('config.json');
+
+    if (!config.basePath) {
+        if (!configFile.basePath) {
+            throw Error('Base path not specified via --base-path');
+        }
+
+        console.log('Using "base-path = ' + configFile.basePath + '"');
+
+        config.basePath = configFile.basePath;
+    }
+
+    if (!config.testPath) {
+        if (!configFile.testPath) {
+            throw Error('Test path not specified via --test-path');
+        }
+
+        console.log('Using "test-path = ' + configFile.testPath + '"');
+
+        config.testPath = configFile.testPath;
+    }
+}
+
+if (options._.length > 0) {
+    dgeni = new Dgeni([
         require('./src/index')({
-            basePath: options['base-path'],
-            testPath: options['test-path'],
+            basePath: config.basePath,
+            testPath: config.testPath,
             sources: options._
         })
     ]);
-    return dgeni.generate();
-} catch(x) {
-    console.log(x.stack);
-    throw x;
+
+    console.log('Generating boilerplate for ' + options._.length + ' components...');
+
+    dgeni.generate();
+}
+
+if (options.save) {
+    console.log('Saving path configuration to "config.json":');
+    console.dir(config);
+
+    jsonfile.writeFile('config.json', config, function(err) {
+        if (err !== null) {
+            console.error(err);
+        }
+    });
 }
