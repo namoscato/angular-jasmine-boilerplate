e5aa8e0a69c65c1e869cb49e05af55d7e9d4327c
Nick Amoscato <nicholas@amoscato.com>
2016-01-09 15:26:09 -0500

    Escape single quotes in method description
    Fixes #2

--- src/processors/description.js
+++ src/processors/description.js
@@ -19,7 +19,11 @@ module.exports = function descriptionProcessor() {
             doc.methods.forEach(function(method) {
                 parts = method.description.split('\n');
 
+                // Lowercase first letter
                 method.descriptionSummary = parts[0].charAt(0).toLowerCase() + parts[0].slice(1);
+
+                // Escape single quotes
+                method.descriptionSummary = method.descriptionSummary.replace(/'/g, "\\'");
 
                 if (parts.length > 1) {
                     method.descriptionSummary += '...';
