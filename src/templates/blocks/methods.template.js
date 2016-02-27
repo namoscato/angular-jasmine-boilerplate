ed092190a58f73d82a67a0fbe98e85cce39b72f0
Nicholas Amoscato <nicholas.amoscato@theresumator.com>
2016-02-27 16:17:40 -0500

    Move controller construction to private construct() method

--- src/templates/blocks/methods.template.js
+++ src/templates/blocks/methods.template.js
@@ -5,6 +5,10 @@
     
     describe('When {$ method.descriptionSummary $}', function() {
         beforeEach(function() {
+            {% if doc.docType == 'controller' -%}
+                construct();
+
+            {% endif -%}
             target.{$ method.name $}();
         });
 
--- src/templates/controller.template.js
+++ src/templates/controller.template.js
@@ -1,14 +1,19 @@
 {% extends "base.template.js" %}
 
 {% block content %}
-    beforeEach(inject(function($controller) {
+    beforeEach(function() {
     {%- for spy in doc.dependencies.spies %}
         {$ spy.variable $} = jasmine.createSpyObj('{$ spy.dependency $}', ['']);
-    {% endfor %}
-        target = $controller('{$ doc.name $}', {
-        {%- for spy in doc.dependencies.spies %}
-            {$ spy.dependency $}: {$ spy.variable $},
-        {%- endfor %}
+    {% endfor -%}
+    });
+
+    function construct() {
+        inject(function($controller) {
+            target = $controller('{$ doc.name $}', {
+            {%- for spy in doc.dependencies.spies %}
+                {$ spy.dependency $}: {$ spy.variable $},
+            {%- endfor %}
+            });
         });
-    }));
+    }
 {% endblock %}
