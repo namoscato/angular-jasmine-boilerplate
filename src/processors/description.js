6d9ba1d43fed57ca917c0a346f504b96cf6e2ce2
Nick Amoscato <nicholas@amoscato.com>
2016-05-14 14:29:00 -0400

    Update unit tests

--- src/processors/description.js
+++ src/processors/description.js
@@ -25,13 +25,15 @@ module.exports = function descriptionProcessor() {
                 doc.methods.forEach(function(method) {
                     parts = method.description.split('\n');
 
-                    summary = parts[0].charAt(0).toLowerCase() + parts[0].slice(1); // Lowercase first letter
+                    summary = parts[0].trim().replace(/'/g, "\\'"); // Trim whitespace and escape single quotes
 
-                    summary = summary.replace(/'/g, "\\'"); // Escape single quotes
+                    if (summary.length > 0) {
+                        spaceIndex = summary.indexOf(' '); // Assume first word is present tense verb
 
-                    spaceIndex = summary.indexOf(' '); // Assume first word is present tense verb
+                        summary = nlp.verb(summary.substring(0, spaceIndex)).conjugate().gerund + summary.substring(spaceIndex);
+                    }
 
-                    method.descriptionSummary = nlp.verb(summary.substring(0, spaceIndex)).conjugate().gerund + summary.substring(spaceIndex);
+                    method.descriptionSummary = summary;
 
                     if (parts.length > 1) {
                         method.descriptionSummary += '...';
